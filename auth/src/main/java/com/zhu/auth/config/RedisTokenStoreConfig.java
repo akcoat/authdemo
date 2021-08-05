0b18eb717e686f676de0819545753b7646957d89
ZhuJW <akcoat@qq.com>
2021-08-05 17:03:57 +0800

    first comment

---
+++ auth/src/main/java/com/zhu/auth/config/RedisTokenStoreConfig.java
@@ -0,0 +1,22 @@
+package com.zhu.auth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class RedisTokenStoreConfig {
+
+    @Resource
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public TokenStore redisTokenStore (){
+        return new RedisTokenStore(redisConnectionFactory);
+    }
+}
---
+++ auth/src/main/java/com/zhu/auth/config/oauth2/OAuth2Config.java
@@ -0,0 +1,83 @@
+package com.zhu.auth.config.oauth2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+    @Resource
+    public PasswordEncoder passwordEncoder;
+
+    @Resource
+    public UserDetailsService kiteUserDetailsService;
+
+    @Resource
+    private AuthenticationManager authenticationManager;
+
+    @Resource
+    private TokenStore redisTokenStore;
+
+    @Resource
+    private DataSource dataSource;
+
+    @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        /**
+         * redis token 方式
+         */
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(kiteUserDetailsService)
+                .tokenStore(redisTokenStore);
+
+    }
+
+    @Bean
+    public ClientDetailsService clientDetails() {
+        return new JdbcClientDetailsService(dataSource);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.withClientDetails(clientDetails());
+    }
+
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        clients.inMemory()
+//                .withClient("order-client")
+//                .secret(passwordEncoder.encode("order-secret-8888"))
+//                .authorizedGrantTypes("refresh_token", "authorization_code", "password")
+//                .accessTokenValiditySeconds(3600)
+//                .scopes("all")
+//                .and()
+//                .withClient("user-client")
+//                .secret(passwordEncoder.encode("user-secret-8888"))
+//                .authorizedGrantTypes("refresh_token", "authorization_code", "password")
+//                .accessTokenValiditySeconds(3600)
+//                .scopes("all");
+//    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.allowFormAuthenticationForClients();
+        security.checkTokenAccess("isAuthenticated()");
+        security.tokenKeyAccess("isAuthenticated()");
+    }
+}
