b6c1d06906c20e3f87f3aa2035b21ff99c525b4d
ZhuJW <akcoat@qq.com>
2021-08-09 15:35:12 +0800

    支持nacos作为注册中心

--- auth/src/main/java/com/zhu/auth/AuthApplication.java
+++ auth/src/main/java/com/zhu/auth/AuthApplication.java
@@ -3,7 +3,9 @@ package com.zhu.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthApplication {
 
